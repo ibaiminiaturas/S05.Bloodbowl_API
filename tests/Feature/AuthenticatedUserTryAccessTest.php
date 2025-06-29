74332b1ac1d9aa5859941b6539901c6003e901bb
Ibai <ibaiminiaturas@gmail.com>
2025-06-29 14:22:07 +0200

    User registration TDD done

--- tests/Feature/AuthenticatedUserTryAccessTest.php
+++ tests/Feature/AuthenticatedUserTryAccessTest.php
@@ -5,27 +5,16 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Models\User;
-use Laravel\Passport\Passport;
 
 class AuthenticatedUserTryAccessTest extends TestCase
 {
-    public function test_authorized_user_can_access_user_route()
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
     {
-        // Creamos un usuario (puedes usar factory)
-        $user = User::factory()->create();
+        $response = $this->get('/');
 
-        // Autenticamos al usuario usando Passport
-        Passport::actingAs($user);
-
-        // Hacemos petición a ruta protegida
-        $response = $this->getJson('/api/user');
-
-        // Debe devolver 200 y los datos del usuario
-        $response->assertStatus(200)
-                 ->assertJson([
-                    'id' => $user->id,
-                    'email' => $user->email,
-                 ]);
+        $response->assertStatus(200);
     }
 }
