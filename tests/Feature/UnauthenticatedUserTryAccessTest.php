8df7973b6e68892d51e6d1b29a4a192bb0ca1fae
Ibai <ibaiminiaturas@gmail.com>
2025-06-29 13:12:23 +0200

    Unauthenticated user access test

---
+++ tests/Feature/UnauthenticatedUserTryAccessTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UnauthenticatedUserTryAccessTest extends TestCase
+{
+    public function test_Unauthenticated_user_cannot_access_user_route()
+    {
+        $response = $this->getJson('/api/user');
+
+        $response->assertStatus(401);
+    }
+}
