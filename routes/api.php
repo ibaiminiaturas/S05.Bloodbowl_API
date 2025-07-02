3dde677928daa6bc547b369258a0e25da8bca366
Ibai <ibaiminiaturas@gmail.com>
2025-07-02 12:53:10 +0200

    Skills TDD and GET done

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\SkillsController;
 
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
@@ -11,8 +12,6 @@ Route::post('/login', [LoginController::class, 'login']);
 
 // Rutas protegidas por autenticación
 Route::middleware(['auth:api', 'token.revoked', 'role:admin'])->group(function () {
-    Route::post('/logout', [LoginController::class, 'logout']);
-
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
@@ -23,13 +22,6 @@ Route::middleware(['auth:api', 'token.revoked', 'role:admin'])->group(function (
 });
 // Rutas protegidas por autenticación
 Route::middleware(['auth:api', 'token.revoked', 'role:admin|coach'])->group(function () {
-    Route::post('/logout', [LoginController::class, 'logout']);
-
-        Route::get('/skills', function (Request $request) {
-        return "{skills: []}";
-    });
-
-
-
-
+        Route::post('/logout', [LoginController::class, 'logout']);
+        Route::get('/skills',[SkillsController::class, 'index']);
 });
