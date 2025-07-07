94086b37fc5f4841737c52784fa9d381a16f0831
Ibai <ibaiminiaturas@gmail.com>
2025-07-07 13:51:59 +0200

    Added check for jersey number

--- tests/Feature/TeamPlayerCreationTest.php
+++ tests/Feature/TeamPlayerCreationTest.php
@@ -18,7 +18,7 @@ class TeamPlayerCreationTest extends TestCase
 {
     use UtilsForTesting;
 
-    public function test_admin_can_create_player_into_team(): void
+    private function createPlayer(bool $coachCreatesPlayer = false, bool $rosterNew = false)
     {
         $admin = $this->getAdminUser();
         Passport::actingAs($admin);
@@ -26,133 +26,87 @@ class TeamPlayerCreationTest extends TestCase
         $response = $this->createTeam($coach->id);
         $team = Team::where('name', 'Test team')->first();
 
+        if ($coachCreatesPlayer)
+        {
+            Passport::actingAs($coach);
+        }
+        
+        if ($rosterNew)
+        {
+            $playerType = 33;
+        }
+        else
+        {
+            $playerType = 1;
+        }
+
         $response = $this->postJson('/api/teams/'. $team->id . '/players',
         [
             'name' => 'Test Player',
             'team_id' => $team->id,
-            'player_type_id' => PlayerType::where('roster_id', $team->roster_id)->first()->id,
+            'player_type_id' => $playerType,
             'player_number' => 1,
             'injuries' => '',
             'spp' => 2
         
         ]);
 
+        return $response;
+    }
+
+
+    public function test_admin_can_create_player_into_team(): void
+    {
+        $response = $this->createPlayer();
         $response->assertStatus(201);
         $player = TeamPlayer::where('name', 'Test Player')->delete();
         $response->assertStatus(201);
-        $team->delete();
+
+
     }
 
 
     public function test_admin_can_not_create_player_into_team_from_different_roster(): void
     {
-        $admin = $this->getAdminUser();
-        Passport::actingAs($admin);
-        $coach = $this->DeleteUserAndCreate();
-        $response = $this->createTeam($coach->id);
-        $team = Team::where('name', 'Test team')->first();
-
-        $response = $this->postJson('/api/teams/'. $team->id . '/players',
-        [
-            'name' => 'Test Player',
-            'team_id' => $team->id,
-            'player_type_id' => PlayerType::where('roster_id', 3)->first()->id,
-            'player_number' => 1,
-            'injuries' => '',
-            'spp' => 2
-        
-        ]);
-
+        $response = $this->createPlayer(false, true);
         $response->assertStatus(422);
         $response->assertJsonValidationErrors('player_type_id');
         $response->assertJsonFragment([
             'player_type_id' => ['The type of player does not belong to the team roster.'],
         ]);
-        $coach->delete();
+
     }
 
     public function test_coach_can_create_player_into_team(): void
     {
-        $admin = $this->getAdminUser();
-        Passport::actingAs($admin);
-        $coach = $this->DeleteUserAndCreate();
-        $response = $this->createTeam($coach->id);
-        $team = Team::where('name', 'Test team')->first();
-        Passport::actingAs($coach);
-
-        $response = $this->postJson('/api/teams/'. $team->id . '/players',
-        [
-            'name' => 'Test Player',
-            'team_id' => $team->id,
-            'player_type_id' => PlayerType::where('roster_id', $team->roster_id)->first()->id,
-            'player_number' => 1,
-            'injuries' => '',
-            'spp' => 2
-        
-        ]);
-
+        $response = $this->createPlayer(true);
         $response->assertStatus(201);
-        $coach->delete();
+
     }
 
     public function test_coach_can_not_create_player_into_team_from_different_roster(): void
     {
-        $admin = $this->getAdminUser();
-        Passport::actingAs($admin);
-        $coach = $this->DeleteUserAndCreate();
-        $response = $this->createTeam($coach->id);
-        $team = Team::where('name', 'Test team')->first();
-        Passport::actingAs($coach);
 
-        $response = $this->postJson('/api/teams/'. $team->id . '/players',
-        [
-            'name' => 'Test Player',
-            'team_id' => $team->id,
-            'player_type_id' => PlayerType::where('roster_id', 3)->first()->id,
-            'player_number' => 1,
-            'injuries' => '',
-            'spp' => 2
-        
-        ]);
-
+        $response = $this->createPlayer(false, true);
         $response->assertStatus(422);
         $response->assertJsonValidationErrors('player_type_id');
         $response->assertJsonFragment([
             'player_type_id' => ['The type of player does not belong to the team roster.'],
         ]);
-        $coach->delete();
+
     }
 
     public function test_coach_can_not_create_player_with_same_name(): void
     {
-
-        //$this->withoutExceptionHandling();
-        $admin = $this->getAdminUser();
-        Passport::actingAs($admin);
-        $coach = $this->DeleteUserAndCreate();
-        $response = $this->createTeam($coach->id);
-        $team = Team::where('name', 'Test team')->first();
-        Passport::actingAs($coach);
-
-        $response = $this->postJson('/api/teams/'. $team->id . '/players',
-        [
-            'name' => 'Test Player',
-            'team_id' => $team->id,
-            'player_type_id' => PlayerType::where('roster_id', $team->roster_id)->first()->id,
-            'player_number' => 1,
-            'injuries' => '',
-            'spp' => 2
-        
-        ]);
-
+        $response = $this->createPlayer(true);
         $response->assertStatus(201);
-
+        $team = Team::where('name', 'Test team')->first();
         $response = $this->postJson('/api/teams/'. $team->id . '/players',
         [
             'name' => 'Test Player',
             'team_id' => $team->id,
-            'player_type_id' => PlayerType::where('roster_id', $team->roster_id)->first()->id,
-            'player_number' => 2,
+            'player_type_id' => $team->roster_id,
+            'player_number' =>2,
             'injuries' => '',
             'spp' => 2
         
@@ -166,7 +120,34 @@ class TeamPlayerCreationTest extends TestCase
             ],
         ]);
 
-        $coach->delete();
+    }
+
+    public function test_coach_can_not_create_player_with_same_number(): void
+    {
+        $response = $this->createPlayer(true);
+
+        $response->assertStatus(201);
+        $team = Team::where('name', 'Test team')->first();
+        $response = $this->postJson('/api/teams/'. $team->id . '/players',
+        [
+            'name' => 'Test Player 2',
+            'team_id' => $team->id,
+            'player_type_id' => PlayerType::where('roster_id', $team->roster_id)->first()->id,
+            'player_number' => 1,
+            'injuries' => '',
+            'spp' => 2
+        
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('player_number');
+        $response->assertJsonFragment([
+            'errors' => [
+                'player_number' => ['The player number has already been taken.'],
+            ],
+        ]);
+
+
     }
 
 
